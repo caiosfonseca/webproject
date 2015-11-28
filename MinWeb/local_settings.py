d12d751ba41cadca7aad3a606c03afb20fcb8682
caiosfonseca <csf2@cin.ufpe.br>
2015-11-28 12:39:08 -0300

    Adicionado os primeiros html e css

--- MinWeb/local_settings.py
+++ MinWeb/local_settings.py
@@ -3,13 +3,6 @@ DEBUG = True
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response, redirect
+from django.contrib.auth.decorators import login_required
+from django.template import RequestContext
+from .models import *
 
 # Create your views here.
+
+@login_required
+def home(request):
+    c = RequestContext(request)
+
+    return render_to_response('index.html', c)
